6947cefb0207973cc4da90179a7fd9c33aaa9bc7
unknown <niteshsainilawar>
2023-01-29 12:45:25 +0530

    first commit

---
+++ src/main/java/com/app/nit/MynewProject123/App.java
@@ -0,0 +1,7 @@
+package com.app.nit.MynewProject123;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/main/java/com/app/nit/MynewProject123/Testng.java
@@ -0,0 +1,22 @@
+package com.app.nit.MynewProject123;
+
+import org.testng.annotations.Test;
+
+public class Testng {
+	@Test
+	public void t1() {
+		System.out.println("test1");
+		
+	}
+	@Test
+	public void t2() {
+		System.out.println("test2");
+		
+	}
+	@Test
+	public void t3() {
+		System.out.println("test3");
+		
+	}
+
+}
